90d03be91c08e30e7b4df110c63d7283f86e83b6
Alejandro Visiedo <avisiedo@redhat.com>
2021-11-29 15:41:47 +0100

    Simplify requirements and update configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'avisiedo'
+AUTHOR = 'tinyeng'
 SITENAME = 'Under the hood'
 SITEURL = ''
 
@@ -20,10 +20,8 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Under The Hood', 'https://avisiedo.github.io'),
+         ('Pelican', 'http://getpelican.com/'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -33,3 +31,6 @@ DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = "./themes/current"
+
